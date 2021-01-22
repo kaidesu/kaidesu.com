fb2326cd689d571ac01adf711ebc18f5801ea20f
Shea Lewis (Kai) <shea.lewis89@gmail.com>
2021-01-21 18:42:03 -0800

    update gtag

--- resources/views/_layout.blade.php
+++ resources/views/_layout.blade.php
@@ -5,12 +5,14 @@
         <!-- Global site tag (gtag.js) - Google Analytics -->
         <script async src="https://www.googletagmanager.com/gtag/js?id=UA-180827913-1"></script>
 
+        <!-- Global site tag (gtag.js) - Google Analytics -->
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-2SYY12VV5E"></script>
         <script>
             window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
             gtag('js', new Date());
 
-            gtag('config', 'UA-180827913-1');
+            gtag('config', 'G-2SYY12VV5E');
         </script>
 
         <meta charset="utf-8">
