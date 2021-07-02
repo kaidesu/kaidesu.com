c50b39c9fac2bf4fd96f27f4e8ce1244aaf23395
Shea Lewis (Kai) <shea.lewis89@gmail.com>
2021-07-01 21:19:27 -0700

    Update footer and header

--- components/TheHeader.vue
+++ components/TheHeader.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="mb-12">
+    <header class="mb-12">
         <div class="max-w-4xl mx-auto px-6 py-3 flex justify-between items-center text-gray-900">
             <a href="/" class="font-bold text-gray-900 font-bold py-1 px-2">kai desu.</a>
 
@@ -9,7 +9,7 @@
                 <a href="/experience" class="mr-3 hover:bg-gray-900 hover:text-gray-100 py-1 px-2 rounded">Experience</a>
             </div>
         </div>
-    </div>
+    </header>
 </template>
 
 <script>
